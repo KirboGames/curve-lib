f355b8d95d6f0242a92f47fdcdcae0a9bcd2dd86
Kirill <kirbo06@mail.ru>
2022-01-22 16:52:08 +0100

    - deleted unused fields and methods

--- src/main/java/com/intkgc/curve/registry/RegistryObject.java
+++ src/main/java/com/intkgc/curve/registry/RegistryObject.java
@@ -3,7 +3,6 @@ package com.intkgc.curve.registry;
 public abstract class RegistryObject {
     protected int id;
     protected String stringId;
-    protected String package_;
 
     public int getId() {
         return id;
@@ -13,20 +12,12 @@ public abstract class RegistryObject {
         return stringId;
     }
 
-    public String getPackage_() {
-        return package_;
-    }
-
     protected void setId(int id) {
         this.id = id;
     }
 
     protected void setStringId(String stringId) {
         this.stringId = stringId;
-    }
-
-    protected void setPackage_(String package_) {
-        this.package_ = package_;
     }
 
     /**
